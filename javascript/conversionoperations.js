08c442bc6c11ccaf6fe4e4dffd4dc37030266d32
Pratik  Gaikwad <128162677+pratikbgaikwad66@users.noreply.github.com>
2024-03-15 14:02:57 +0000

    container config done

---
+++ javascript/conversionoperations.js
@@ -0,0 +1,36 @@
+const score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+    
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedin = "978"
+
+let booleanIsloggedIn = Boolean(isLoggedin)
+// console.log(booleanIsloggedIn);
+
+
+let somenum = 33 
+
+let stringNumber = String(somenum)
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+// operations 
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+
+let gamecounter = 100
+
+++gamecounter;
+console.log(gamecounter);
+
---
+++ javascript/variables.js
@@ -0,0 +1,5 @@
+const accountId = "784784"
+let accountemail = "pratik@89389"
+var accountState = "goa"
+
+console.table([accountId, accountemail, accountState])
