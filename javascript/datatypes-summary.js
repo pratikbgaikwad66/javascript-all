aba4eea359270ec52f894c106e347fc044cb1ad7
Pratik  Gaikwad <128162677+pratikbgaikwad66@users.noreply.github.com>
2024-03-15 14:48:58 +0000

    container config done

---
+++ javascript/datatypes-summary.js
@@ -0,0 +1,27 @@
+//  primitive data types 
+
+const score = 100 
+const scoreValue = 100.3
+
+const isLoggedin = false 
+const outsideTemp = null 
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol("123")
+
+console.log(id === anotherId);
+
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name : "hitesh",
+    age : 22,
+}
+
+const myfunction = function() {
+    console.log("hello world");
+}
+
+// console.log(typeof anotherId);
