2290efcd06d98fea5439d91efb019ef544044ebf
Pratik  Gaikwad <128162677+pratikbgaikwad66@users.noreply.github.com>
2024-03-18 15:18:52 +0000

    container config done

---
+++ javascript/comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log( 2==1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log(1 === 1 );
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log( null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// console.log("2" === 2);
+
+
+let x = 20;
+let y = 20;
+console.log(x == y);
---
+++ javascript/datatypestack.js
@@ -0,0 +1,8 @@
+let pratikyoutube = "frontendyoutube"
+
+let mycoder = "pratikyoutube"
+
+mycode ="pratikgaikwad";
+
+console.log(pratikyoutube);
+console.log(mycoder);
